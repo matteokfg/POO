a2fad35fc489658bacb1584857cfaec5c7e8c1d1
Raul Abreu <raulguilhermesalles@hotmail.com>
2023-11-04 15:46:54 -0300

    teste

--- classes/menu.py
+++ classes/menu.py
@@ -1,4 +1,5 @@
 from login import Login
+from util import *
 
 
 
@@ -6,14 +7,28 @@ class Menu:
     
     def __init__(self):
         
-        pass
+        self.mostrar_opcoes_inicial()
 
     def mostrar_opcoes_inicial(self):
-        print("O que deseja fazer?\n1-Fazer Login\n2-Cadastrar-se\n3-Sair")
+        while True:
+            print("O que deseja fazer?\n1-Fazer Login\n2-Cadastrar-se\n3-Sair")
+            opcao = input()
+            if validate_inteiro(opcao):
+                if opcao == "1":
+                    print("Login")
+                    break
+                elif opcao == "2":
+                    print("Cadastrar-se")
+                    break
+                elif opcao == "3":
+                    break
+            else:
+                self.opcao_invalida()
+
 
     def opcao_invalida(self):
         print("Opção inválida!")
     
 if __name__ == "__main__":
     
-    Menu()
+    menu = Menu()
