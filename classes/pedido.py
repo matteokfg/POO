0e0184365ea84967262dbb9130ddfb026b39cfda
matteokfg <67129306+matteokfg@users.noreply.github.com>
2023-11-10 21:44:16 -0300

    Update pedido.py

---
+++ classes/pedido.py
@@ -0,0 +1,9 @@
+from connector import Connector
+
+class Pedido(Connector):
+    def __init__(self, codigo, codigos_produtos, quantidades, codigo_comprador, codigos_lojas):
+        self.__codigo = codigo
+        self.__codigos_produtos = codigos_produtos
+        self.__quantidades = quantidades
+        self.__codigo_comprador = codigo_comprador
+        self.__codigos_lojas = codigos_lojas
