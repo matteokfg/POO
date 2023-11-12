9494785f3312d7ca48fbf800eaa61f6af3a9bb9a
matteokfg <67129306+matteokfg@users.noreply.github.com>
2023-11-12 14:15:09 -0300

    Getters and setters - comentario, pedido, produto

--- classes/comentario.py
+++ classes/comentario.py
@@ -9,6 +9,46 @@ class Comentario(Connector):
         self.__codigo_loja = codigo_loja
         self.__descricao = descricao
 
+    @property
+    def codigo(self):
+        return self.__codigo
+
+    @codigo.setter
+    def codigo(self, novo_codigo):
+        self.__codigo = novo_codigo
+
+    @property
+    def codigo_comprador(self):
+        return self.__codigo_comprador
+
+    @codigo_comprador.setter
+    def codigo_comprador(self, novo_codigo_comprador):
+        self.__codigo_comprador = novo_codigo_comprador
+
+    @property
+    def codigo_produto(self):
+        return self.__codigo_produto
+
+    @codigo_produto.setter
+    def codigo_produto(self, novo_codigo_produto):
+        self.__codigo_produto = novo_codigo_produto
+
+    @property
+    def codigo_loja(self):
+        return self.__codigo_loja
+
+    @codigo_loja.setter
+    def codigo_loja(self, novo_codigo_loja):
+        self.__codigo_loja = novo_codigo_loja
+
+    @property
+    def descricao(self):
+        return self.__descricao
+
+    @descricao.setter
+    def descricao(self, novo_descricao):
+        self.__descricao = novo_descricao
+
     def comentarios_produto(self):
         pass
 
--- classes/produto.py
+++ classes/produto.py
@@ -11,3 +11,67 @@ class Produto(Connector):
         self.__tipo = tipo
         self.__marca = marca
         self.__codigo_loja = codigo_loja
+
+    @property
+    def codigo(self):
+        return self.__codigo
+    
+    @codigo.setter
+    def codigo(self, novo_codigo):
+        self.__codigo = novo_codigo
+
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome
+
+    @property
+    def descricao(self):
+        return self.__descricao
+    
+    @descricao.setter
+    def descricao(self, novo_descricao):
+        self.__descricao = novo_descricao
+
+    @property
+    def imagem(self):
+        return self.__imagem
+    
+    @imagem.setter
+    def imagem(self, novo_imagem):
+        self.__imagem = novo_imagem
+
+    @property
+    def preco_unitario(self):
+        return self.__preco_unitario
+    
+    @preco_unitario.setter
+    def preco_unitario(self, novo_preco_unitario):
+        self.__preco_unitario = novo_preco_unitario
+
+    @property
+    def tipo(self):
+        return self.__tipo
+    
+    @tipo.setter
+    def tipo(self, novo_tipo):
+        self.__tipo = novo_tipo
+
+    @property
+    def marca(self):
+        return self.__marca
+    
+    @marca.setter
+    def marca(self, novo_marca):
+        self.__marca = novo_marca
+
+    @property
+    def codigo_loja(self):
+        return self.__codigo_loja
+    
+    @codigo_loja.setter
+    def codigo_loja(self, novo_codigo_loja):
+        self.__codigo_loja = novo_codigo_loja
